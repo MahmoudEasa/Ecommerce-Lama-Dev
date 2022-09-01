99d738aee47277c56dbc01beb1654b582f07f236
Mahmoud Easa <mu01011422865@gmail.com>
2022-09-01 03:39:22 +0200

    API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import Pay from "./reactStripe/Pay";
+import Success from "./reactStripe/Success";
 
 const App = () => {
   return (
-    <div>
-      <Home />
+    <Routes>
+      <Route path="/pay" element={<Pay />} />
+      <Route path="/success" element={<Success />} />
+      {/* <Home /> */}
       {/* <ProductList /> */}
       {/* <Product /> */}
       {/* <Register /> */}
       {/* <Login /> */}
       {/* <Cart /> */}
-    </div>
+    </Routes>
   );
 };
 
