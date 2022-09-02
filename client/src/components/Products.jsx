e51b04dc0073db4c8c7caedec1a767f0ac3b8773
Mahmoud Easa <mu01011422865@gmail.com>
2022-09-02 19:04:55 +0200

    Edit

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import styled from "styled-components";
 import Product from "./Product";
 import { popularProducts } from "../data";
@@ -9,12 +11,37 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
-const Products = () => {
+const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setfilteredProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        cat
+          ? `http://localhost:5000/api/products?category=${cat}`
+          : "http://localhost:5000/api/products"
+      )
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.log(err));
+  }, [cat]);
+
+  useEffect(() => {
+    cat &&
+      setfilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [cat, filters, products]);
+
   return (
     <Container>
-      {popularProducts.map((item) => (
-        <Product key={item.id} item={item} />
-      ))}
+      {cat
+        ? filteredProducts.map((item) => <Product key={item.id} item={item} />)
+        : products.map((item) => <Product key={item.id} item={item} />)}
     </Container>
   );
 };
--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
 import Products from "./../components/Products";
@@ -41,6 +43,19 @@ const Select = styled.select`
 const Option = styled.option``;
 
 const ProductList = () => {
+  const location = useLocation();
+  const cat = location.pathname.split("/")[2];
+  const [filters, setFilters] = useState({});
+  const [sort, setSort] = useState("Newest");
+
+  const handleFilters = (e) => {
+    const value = e.target.value;
+    setFilters({
+      ...filters,
+      [e.target.name]: value,
+    });
+  };
+
   return (
     <Container>
       <Navbar />
@@ -49,10 +64,8 @@ const ProductList = () => {
       <FilterContainer>
         <Filter>
           <FilterText>Filter Products:</FilterText>
-          <Select>
-            <Option disabled selected>
-              Color
-            </Option>
+          <Select defaultValue="Color" name="color" onChange={handleFilters}>
+            <Option disabled>Color</Option>
             <Option>White</Option>
             <Option>Black</Option>
             <Option>Red</Option>
@@ -60,10 +73,8 @@ const ProductList = () => {
             <Option>Yellow</Option>
             <Option>Green</Option>
           </Select>
-          <Select>
-            <Option disabled selected>
-              Size
-            </Option>
+          <Select defaultValue="Size" name="size" onChange={handleFilters}>
+            <Option disabled>Size</Option>
             <Option>XS</Option>
             <Option>S</Option>
             <Option>M</Option>
@@ -73,14 +84,14 @@ const ProductList = () => {
         </Filter>
         <Filter>
           <FilterText>Sort Products:</FilterText>
-          <Select>
-            <Option selected>Newest</Option>
-            <Option>Price (asc)</Option>
-            <Option>Price (desc)</Option>
+          <Select defaultValue={sort} onChange={(e) => setSort(e.target.value)}>
+            <Option value="newest">Newest</Option>
+            <Option value="asc">Price (asc)</Option>
+            <Option value="desc">Price (desc)</Option>
           </Select>
         </Filter>
       </FilterContainer>
-      <Products />
+      <Products cat={cat} filters={filters} sort={sort} />
       <Newsletter />
       <Footer />
     </Container>
